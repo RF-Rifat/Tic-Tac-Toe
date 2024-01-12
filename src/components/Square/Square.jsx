4a33891511a496539f1d42bf8206dfd351aec7b5
RF-Rifat <rfrifat6344@gmail.com>
2024-01-12 15:19:16 +0600

    x value added by index number

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,24 +1,84 @@
 import { useState } from "react";
-import Button from "./Button";
 
 const Square = () => {
   const [value, setValue] = useState(Array(9).fill(0));
+  const handleButtonClick = (index) => {
+    const newValues = value.slice();
+    newValues[index] = "X";
+    console.log("Square index:", index);
+    setValue(newValues);
+  };
+
   return (
     <>
       <div className="flex">
-        <Button value={value[0]} />
-        <Button value={value[1]} />
-        <Button value={value[2]} />
+        <button
+          className="bg-white border border-zinc-500 h-20 w-20 m-1 leading-9 text-lg shadow-xl hover:scale-105 hover:shadow-2xl transition-all duration-100"
+          value={value[0]}
+          onClick={() => handleButtonClick(0)}
+        >
+          {value[0]}
+        </button>
+        <button
+          className="bg-white border border-zinc-500 h-20 w-20 m-1 leading-9 text-lg shadow-xl hover:scale-105 hover:shadow-2xl transition-all duration-100"
+          value={value[1]}
+          onClick={() => handleButtonClick(1)}
+        >
+          {value[1]}
+        </button>
+        <button
+          className="bg-white border border-zinc-500 h-20 w-20 m-1 leading-9 text-lg shadow-xl hover:scale-105 hover:shadow-2xl transition-all duration-100"
+          value={value[2]}
+          onClick={() => handleButtonClick(2)}
+        >
+          {value[2]}
+        </button>
       </div>
       <div className="flex">
-        <Button value={value[3]} />
-        <Button value={value[4]} />
-        <Button value={value[5]} />
+        <button
+          className="bg-white border border-zinc-500 h-20 w-20 m-1 leading-9 text-lg shadow-xl hover:scale-105 hover:shadow-2xl transition-all duration-100"
+          value={value[3]}
+          onClick={() => handleButtonClick(3)}
+        >
+          {value[3]}
+        </button>
+        <button
+          className="bg-white border border-zinc-500 h-20 w-20 m-1 leading-9 text-lg shadow-xl hover:scale-105 hover:shadow-2xl transition-all duration-100"
+          value={value[4]}
+          onClick={() => handleButtonClick(4)}
+        >
+          {value[4]}
+        </button>
+        <button
+          className="bg-white border border-zinc-500 h-20 w-20 m-1 leading-9 text-lg shadow-xl hover:scale-105 hover:shadow-2xl transition-all duration-100"
+          value={value[5]}
+          onClick={() => handleButtonClick(5)}
+        >
+          {value[5]}
+        </button>
       </div>
       <div className="flex">
-        <Button value={value[6]} />
-        <Button value={value[7]} />
-        <Button value={value[8]} />
+        <button
+          className="bg-white border border-zinc-500 h-20 w-20 m-1 leading-9 text-lg shadow-xl hover:scale-105 hover:shadow-2xl transition-all duration-100"
+          value={value[6]}
+          onClick={() => handleButtonClick(6)}
+        >
+          {value[6]}
+        </button>
+        <button
+          className="bg-white border border-zinc-500 h-20 w-20 m-1 leading-9 text-lg shadow-xl hover:scale-105 hover:shadow-2xl transition-all duration-100"
+          value={value[7]}
+          onClick={() => handleButtonClick(7)}
+        >
+          {value[7]}
+        </button>
+        <button
+          className="bg-white border border-zinc-500 h-20 w-20 m-1 leading-9 text-lg shadow-xl hover:scale-105 hover:shadow-2xl transition-all duration-100"
+          value={value[8]}
+          onClick={() => handleButtonClick(8)}
+        >
+          {value[8]}
+        </button>
       </div>
     </>
   );
